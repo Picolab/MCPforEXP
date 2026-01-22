faad179f2afb5d1a9aae7382bc5b5d7f1a1993da
Chance Carr <ccarr16@byu.edu>
2026-01-22 10:54:33 -0700

    Alter code for getting initial ECI, experiment with new helper functions

--- api-wrapper.test.js
+++ api-wrapper.test.js
@@ -1,18 +1,10 @@
-const { get_manifold_pico } = require('./api-wrapper');
+const { getInitialECI } = require('./api-wrapper');
 
-// test('get_manifold_pico with valid ECI', async () => {
-//     try {
-//         const data = await get_manifold_pico("cmklhoq6x002gxgeuaann7tb0");
-//         console.log(data);
-//         expect(data.name).toBe('Manifold');
-//         expect(data.eci).toBeDefined();
-//         expect(data.parent_eci).toBeDefined();
-//     } catch (error) {
-//         throw error;
-//     }
-// })
-
-test('get_manifold_pico with invalid ECI', async () => {
-    const data = await (get_manifold_pico("invalid ECI"));
-    expect(data.error).toBe('Error: ECI not found invalid ECI');
+test('get initial ECI', async () => {
+    try {
+        const eci = await getInitialECI();
+        expect(eci).toBeDefined();
+    } catch (error) {
+        throw error;
+    }
 })
