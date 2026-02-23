d861fe176da0055266cbb94421877f7ff7bd12c9
Micaela Madariaga <madariagamica20@gmail.com>
2026-02-23 10:20:22 -0700

    Fixed setup script

--- scripts/install-manifold.js
+++ scripts/install-manifold.js
@@ -1,3 +1,5 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 const { getRootECI } = require("../src/backend/utility/eci-utility.js");
 const { setupRegistry } = require("../src/backend/utility/api-utility.js");
 
